caea8446d9dacc73bf3d2edde57ceda8065060ab
Mutugi <colrium@gmail.com>
2024-02-26 17:00:12 +0300

    final changes

--- src/api/controllers/ghg/calculation.controller.js
+++ src/api/controllers/ghg/calculation.controller.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import GhgController from '../../base/GhgController.js';
+import Activity from '../../models/ghg/activity.model.js';
 import ActivityType from '../../models/ghg/activityType.model.js';
 import Calculation from '../../models/ghg/calculation.model.js';
 import Domain from '../../models/ghg/domain.model.js';
@@ -34,7 +35,7 @@ class CalculationController extends GhgController {
 		return subjectdata;
 	};
 
-	list = async (req, res, next) => {
+	/* list = async (req, res, next) => {
 		const ContextModel = this.model;
 		const loadSubjectQuery = CalculationController.subjectFilter;
 		let query = { ...req.query };
@@ -50,7 +51,7 @@ class CalculationController extends GhgController {
 		} catch (error) {
 			next(error);
 		}
-	};
+	}; */
 	overview = async (req, res, next) => {
 		const user = req.user;
 		let userId = user?._id || user?.id;
@@ -60,11 +61,35 @@ class CalculationController extends GhgController {
 		const subjectQuery = await CalculationController.subjectFilter(req);
 		const q = { ...req.query, ...subjectQuery };
 		try {
+			const subject = 'domainId';
+			const user = req.user;
+			let userId = user?._id || user?.id;
+			const domains = await Domain.find({
+				userId: user?.role === 'admin' && 'userId' in req.query ? req.query[subject] : { $in: [null, userId] }
+			});
+			const activities = await Activity.find({
+				userId: user?.role === 'admin' && 'userId' in req.query ? req.query[subject] : { $in: [null, userId] }
+			});
+			const domainCount = await Domain.countDocuments({
+				userId: user?.role === 'admin' && 'userId' in req.query ? req.query[subject] : { $in: [null, userId] }
+			})
 			const findQ = Calculation.parseQuery({...q});
+
 			const activityTypeDocs = await ActivityType.find({}).lean();
 			const activityTypeLabels = activityTypeDocs.reduce((acc, curr) => {
 				acc[curr.name] = curr.label;
 				return acc
+			}, {});
+			const domainLabels = domains.reduce((acc, curr) => {
+				acc[curr._id.toString()] = curr.name;
+				return acc;
+			}, {});
+			const activityCount = await Activity.countDocuments({
+				userId: user?.role === 'admin' && 'userId' in req.query ? req.query[subject] : { $in: [null, userId] }
+			});
+			const activitiesLabels = activities.reduce((acc, curr) => {
+				acc[curr.name] = curr.name;
+				return acc;
 			}, {});
 			const docs = await findQ.lean().exec();
 			const data = docs.reduce(
@@ -72,8 +97,8 @@ class CalculationController extends GhgController {
 					const {
 						byScope: { scope1 = 0, scope2 = 0, scope3us = 0, scope3ds = 0 },
 						byemissionType: { biogenic = 0, fossil = 0 },
-						activities={},
-						activityTypes={}
+						activities = {},
+						activityTypes = {}
 					} = {
 						byScope: {
 							scope1: 0,
@@ -91,24 +116,36 @@ class CalculationController extends GhgController {
 					if (typeof acc.yearly[curr.year] !== 'number' || isNaN(acc.yearly[curr.year])) {
 						acc.yearly[curr.year] = 0;
 					}
+					if (curr.domainId) {
+						const domainName = domainLabels[curr.domainId.toString()]
+						const currDomainTotal = acc.domains[domainName] || 0
+						acc.domains[domainName] = currDomainTotal + resTotal;
+					}
 					
+
 					if (curr.activities) {
 						for (const [scope, entries] of Object.entries(curr.activities)) {
-							for (const entry of entries) {
-								const currActivityTotal = activities[entry.name] || 0;
-								activities[entry.name] = currActivityTotal + entry.emission;
-								if (Array.isArray(entry.section)) {
-									for (const activityType of entry.section) {
-										const currActivityTypeTotal = activityTypes[activityType] || 0;
-										const activityTypeLabel = activityTypeLabels[activityType] || activityType;
-										activityTypes[activityTypeLabel] = currActivityTypeTotal + entry.emission;
+							for (const [name, scopeEntries] of Object.entries(entries)) {
+								for (const scopeEntry of scopeEntries) {
+									if (scopeEntry.emissionType) {
+										const emissionTypeTotal = acc.emissionType[scopeEntry.emissionType] || 0;
+										acc.emissionType[scopeEntry.emissionType] = emissionTypeTotal + scopeEntry.emission;
 									}
+									if (scopeEntry.activityType) {
+										const activityTypeLabel = activityTypeLabels[scopeEntry.activityType] || scopeEntry.activityType;
+										const activityTypeTotal = acc.activityTypes[activityTypeLabel] || 0;
+										acc.activityTypes[activityTypeLabel] = activityTypeTotal + scopeEntry.emission;
+									}
+									
+									const activityLabel = activitiesLabels[scopeEntry.name] || scopeEntry.name;
+									const currActivityTotal = activities[activityLabel] || 0;
+									const activityEmission = scopeEntry.emission || 0;
+									activities[activityLabel] = currActivityTotal + activityEmission;
 								}
-								
 							}
 						}
 						acc.activities = activities;
-						acc.activityTypes = activityTypes;
+						// acc.activityTypes = activityTypes;
 					}
 					acc.scope.scope1 += scope1;
 					acc.scope.scope2 += scope2;
@@ -118,16 +155,20 @@ class CalculationController extends GhgController {
 					acc.emissionType.fossil += fossil;
 					acc.yearly[curr.year] += resTotal;
 					acc.total += resTotal;
-					
+
 					return acc;
 				},
 				{
+					domains: {},
 					activities: {},
 					activityTypes: {},
 					yearly: {},
 					scope: { scope1: 0, scope2: 0, scope3us: 0, scope3ds: 0 },
 					emissionType: { biogenic: 0, fossil: 0 },
-					total: 0
+					total: 0,
+					domainCount: domainCount,
+					calculationCount: docs.length,
+					activityCount: activityCount
 				}
 			);
 			res.json(data);
